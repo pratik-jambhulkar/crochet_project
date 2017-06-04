d4e2911d7895f9ed5b5d863ffb5b82320928ce81
Pratik Jambhulkar <pratikj009@gmail.com>
2017-06-04 21:56:01 +0530

    added functional tests as an app to django

--- bags/tests.py
+++ bags/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
 class FirstTestCase(TestCase):
 
-	def test_addition(self):
-		self.assertEqual(2,2)
+    def test_addition(self):
+        self.assertEqual(2, 2)
--- crochet/settings.py
+++ crochet/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bags',
+    'functional_tests',
 ]
 
 MIDDLEWARE = [
