112048ec3aa25e4789399542daf78e84c54a589d
Pratik Jambhulkar <pratikj009@gmail.com>
2017-06-04 23:59:48 +0530

    testing postgresql database connectivity

--- crochet/settings.py
+++ crochet/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'crochet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '',
     }
 }
 
